ed6272c8f0af148b2df7d0ac0c69ccc6a0e72312
Nolan Darilek <nolan@thewordnerd.info>
2019-12-16 16:34:32 -0600

    Tweaks to player input.
     * Make player=s speed == delta.
     * Cap, rather than increment, speed.
     * Don't tweak values before printing them so I know actual screen coordinates.

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -2,31 +2,25 @@ extends Node2D
 
 export var rotation_speed_degrees = 90
 
-var speed = 0
-
 func _process(delta):
   if Input.is_action_just_pressed("speak_coordinates"):
     var x = int(position.x)
     var y = int(position.y)
-    y *= -1
-    if y == -0:
-      y = 0
-    print("%s, %s" % [x, y], true)
+    print("%s, %s" % [x, y])
   elif Input.is_action_just_pressed("speak_heading"):
     var degrees = global_rotation_degrees
-    print("%s degrees" % round(degrees), true)
+    print("%s degrees" % round(degrees))
 
 
 func _physics_process(delta):
+  var speed = 0
   var rotation_dir = 0
   if Input.is_action_pressed("ui_left"):
     rotation_dir -= 1
   elif Input.is_action_pressed("ui_right"):
     rotation_dir += 1
   if Input.is_action_pressed("ui_up"):
-    speed -= delta
-  if Input.is_action_pressed("ui_down"):
-    speed += delta
+    speed = delta
   var rotation_delta = rotation_speed_degrees * rotation_dir * delta 
   global_rotation_degrees += rotation_delta
   var velocity = Vector2(0, speed).rotated(rotation)
